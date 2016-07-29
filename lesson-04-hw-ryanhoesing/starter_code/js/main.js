0b47ea029808bc2ee73d63d84c70f4946d509d5e
John Serrao <bigbadserrao@gmail.com>
2016-07-29 01:17:40 -0400

    guidance on main.js slideDown() method

--- lesson-04-hw-ryanhoesing/starter_code/js/main.js
+++ lesson-04-hw-ryanhoesing/starter_code/js/main.js
@@ -11,12 +11,23 @@ $("a").click(function(event) {
 	event.preventDefault();
 });
 
+// Your code
 $("p.readmore").click(function() {
 	$("p.readmore").hide();
 		$("#show-this-on-click").slideDown(function(){
 	  $("p.readless").show();
 		});	
 });	
+
+// Answer code
+// Note that slideDown doesn't need an anonymous function to work right
+// This is why your code skips when you click - you were super close to getting this perfect though
+  $('.readmore').click(function(){
+    $('#show-this-on-click').slideDown();
+    $('.readmore').hide();
+    $('.readless').show();
+    event.preventDefault();
+  });
 
 $("p.readless").click(function(){
 	$("p.readless").hide();

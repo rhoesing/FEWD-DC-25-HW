661b6bb0dafa0b641dfd00fd78570e83b78156fd
rhoesing <rdhoesing@gmail.com>
2016-07-29 09:22:16 -0400

    Update

---
+++ lesson-05-hw-ryanhoesing/starter_code/js/main.js
@@ -0,0 +1,24 @@
+/*
+Project Name: lesson-05-hw-ryanhoesing
+Client: CitiPix
+Author: Ryan Hoesing
+Developer @GA in DC
+*/
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //closing out READY
